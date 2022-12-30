39aee884ad1cd48a69aa55a71523311c1df5dc5f
marcantonio99 <marcantonio8799@gamil.com>
2022-12-30 10:35:13 +0100

    restart

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once __DIR__ . '/db/index.php';
+
+include_once __DIR__ . '/partial/head.php';
+
+// include portion where I write:
+include_once __DIR__ . '/partial/body.php';
+
+include_once __DIR__ . '/partial/foot.php';
+
+?>
---
+++ partial/foot.php
@@ -0,0 +1,11 @@
+
+    <!--include Vue-->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <!--include Axios-->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!--include Bootstrap's JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!--link my script-->
+    <script src="./assets/scripts/main.js"></script>
+</body>
+</html>
